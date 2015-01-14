7271ccb8263c60b50bdc557382b88e2f33ed3649
popeen <patrik@ptjwebben.se>
2015-01-14 17:04:46 +0100

    Updated the text and "design" of the API description

--- index.php
+++ index.php
@@ -19,15 +19,16 @@
 		<title>SMSGrupp API</title>
 		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 		<style type="text/css">
-			*{ font-size:20px; font-family:'Lato',sans-serif; }
-			a{ color:#666; text-decoration:none; font-weight:bold; }
+			*{ font-size:20px; font-family:'Lato',sans-serif; background-color:#ccc;}
+			a{ color:#606aaa; text-decoration:none; font-weight:bold; }
 		</style>
 	</head>
 	<body>
-		Att skicka SMS med <b>Kakadua SMSGrupp API</b> är enkelt. Skapa bara en <a href="https://smsgrupp.se/">SMSGrupp</a> och bjud in vårt nummer (<b><?php echo $phone_number; ?></b>) till den.<br/>
+		Att skicka SMS med <b>SMSGrupp API</b> är enkelt. Skapa bara en <a href="https://smsgrupp.se/" target="_blank">SMSGrupp</a> och bjud in vårt nummer (<b><?php echo $phone_number; ?></b>) till den.<br/>
 		Sen skickar du sms genom att göra en GET request mot <?php echo get_url_directory();?>?grp=<b>{GRUPP_ID}</b>&msg=<b>{MSG}</b><br/>&nbsp;<br/>
-		<b>{GRUPP_ID}</b> =  Du ser det i adressen till din grupp på <a href="https://smsgrupp.se/">smsgrupp.se</a>. I det här exemplet är det 123456, http://smsgrupp.se/grupp/sapp/123456)<br/>
+		<b>{GRUPP_ID}</b> =  Du ser det i adressen till din grupp på <a href="https://smsgrupp.se/" target="_blank">smsgrupp.se</a>. I det här exemplet är det 123456, http://smsgrupp.se/grupp/sapp/123456)<br/>
 		<b>{MSG}</b> = Det meddelande du vill skicka, se till att det är URL encodat<br/>&nbsp;<br/>
-		APIet bygger på våran <b>SMSGrupp in PHP</b> klass som finns publicerad på <a href="https://github.com/Kakadua/SMSGrupp-in-PHP">den här GitHub sidan</a>
+		APIet är Open Source och kan laddas ner på <a href="https://github.com/Kakadua/SMSGrupp-in-PHP/tree/API" target="_blank">den här GitHub sidan</a>.<br/>
+		Det bygger på våran <b>SMSGrupp in PHP</b> klass som finns publicerad på <a href="https://github.com/Kakadua/SMSGrupp-in-PHP/tree/master" target="_blank">den här GitHub sidan</a>
 	</body>
 </html>
